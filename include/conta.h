129471c72a24a1ffd1e53448d98c3fea6a76d931
samira malaquias <samiraams@yahoo.com.br>
2023-07-02 21:37:00 -0300

    aa

---
+++ include/conta.h
@@ -0,0 +1,19 @@
+#ifndef CONTA_HPP
+#define CONTA_HPP
+
+#include <iostream>
+#include "comanda.h"
+using namespace std;
+
+class Conta{
+    private:
+    Comanda* _comanda;
+    double valorTotal;
+
+    
+}
+
+
+
+
+#endif
