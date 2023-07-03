d93cabac8aab06fee8bc302111bfa7fd3c7df90d
unknown <barbararamosberno@outlook.com>
2023-07-03 11:54:14 -0300

    criação  estoque.h

--- include/estoque.h
+++ include/estoque.h
@@ -1,14 +1,27 @@
 #ifndef ESTOQUE_H
 #define ESTOQUE_H
 
-#include "cadastro.h"
-#include "cardapio.h"
-#include <vector>
+#include "../include/comanda.h"
+#include "../include/cardapio.h"
 #include <iostream>
+#include <unordered_map>
 #include <string>
 using namespace std;
 
+class Estoque{
+  private:
+    unordered_map<string, int> estoque;
 
+    public:
+    void adicionar_estoque(string item, int quantidade);
+
+    void remover_estoque(string item, int quantidade);
+}
 
 
 #endif
+
+
+
+
+
