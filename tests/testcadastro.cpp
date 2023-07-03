8753eca26416281de0374446a8ea94506a655d80
unknown <barbararamosberno@outlook.com>
2023-07-03 15:49:46 -0300

    iniciando testes de cadastro

---
+++ tests/testcadastro.cpp
@@ -0,0 +1,44 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define TEST_CASE 
+#include "../third_party/doctest.h"
+#include "../include/cadastro.h"
+
+TEST_CASE("Teste do construtor Cadastro") {
+    // Teste 1: ID inválido
+    SUBCASE("ID inválido") {
+        CHECK_THROWS_AS(Cadastro(-1, "João", "12345", "12345678910"), std::invalid_argument);
+    }
+TEST_CASE("Teste ValidarCPF") {
+    Cadastro cliente("id", "nome", "telefone", "123456789");
+    CHECK(Cadastro.GetCPF() == "123456789");
+}
+
+
+
+// Testes de unidade usando doctest
+TEST_CASE("Teste do construtor Cadastro") {
+    // Teste 1: ID inválido
+    SUBCASE("ID inválido") {
+        CHECK_THROWS_AS(Cadastro(-1, "João", "12345", "12345678910"), id_invalido_e());
+    }
+
+    // Teste 2: Nome vazio
+    SUBCASE("Nome vazio") {
+        CHECK_THROWS_AS(Cadastro(1, "", "12345", "12345678910"), nome_invalido_e());
+    }
+
+    // Teste 3: Telefone vazio
+    SUBCASE("Telefone vazio") {
+        CHECK_THROWS_AS(Cadastro(1, "João", "", "12345678910"), telefone_invalido_e());
+    }
+
+    // Teste 4: CPF inválido
+    SUBCASE("CPF inválido") {
+        CHECK_THROWS_AS(Cadastro(1, "João", "12345", "123456"), cpf_invalido_e());
+    }
+
+    // Teste 5: Dados válidos
+    SUBCASE("Dados válidos") {
+        CHECK_NOTHROW(Cadastro(1, "João", "12345", "12345678910"));
+    }
+}
