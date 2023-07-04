9dcff5304c4f8fcf2c55b56d5e53d0c5b7bfd948
Marcelo-Hochreiter <64548509+Marcelo-Hochreiter@users.noreply.github.com>
2023-07-04 08:29:41 -0300

    Update menu.cpp

--- src/menu.cpp
+++ src/menu.cpp
@@ -11,8 +11,17 @@ void Menu::MenuStart()
 
 void Menu::MenuGerente()
 {
+        std::cout << "MENU GERENTE" << std:: endl;
+    std::cout << "(1)- Adicionar produto ao estoque." << std::endl;
+    std::cout << "(2)- Remover produto do estoque." << std::endl;
+    std::cout << "(3)- Exibir extrato do caixa." << std::endl;
+    std::cout << "(4)- Adicionar Prato Entrada" << std::endl;
+    std::cout << "(5)- Adicionar Prato Principal" << std::endl;
+    std::cout << "(6)- Adicionar Prato Sobremesa." << std::endl;
+    std::cout << "(7)- Exibir Pedidos." << std::endl;
+    std::cout << "(0)- Sair" << std::endl;
+    std::cout << "Digite sua opção: ";
 
-    
 }
 
 void Menu::MenuCliente()
@@ -29,6 +38,7 @@ void Menu::MenuCliente()
     std::cout << "(9)- Exibir valor total com gorjeta." << std::endl;
     std::cout << "(10)- Efetuar pagamento" << std::endl;
     std::cout << "(11)- Informar pagamento efetuado " << std::endl;
+    std::cout << "(0)- Sair" << std::endl;
     std::cout << "Digite sua opção: ";
 
 
